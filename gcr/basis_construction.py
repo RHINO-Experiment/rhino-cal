0b61451b9f354c33534d9933caf2300cc561b5a2
jmnorris98 <jmnorris98@gmail.com>
2026-03-30 16:30:58 +0100

    Vectorised basis function for surface reconstruction

--- gcr/basis_construction.py
+++ gcr/basis_construction.py
@@ -29,6 +29,26 @@ def construct_basis(x:np.ndarray,
     return basis
 
 
+def construct_basis_vectorised(x: np.ndarray,
+                    y: np.ndarray,
+                    n_x_coeffs: int,
+                    n_y_coeffs: int):
+
+    xx, yy = np.meshgrid(x, y)
+
+    x_flat = xx.ravel()
+    y_flat = yy.ravel()
+
+    Vx = np.vander(x_flat, n_x_coeffs)
+    Vy = np.vander(y_flat, n_y_coeffs)
+
+    # Swap order to match original implementation
+    basis = (Vy[:, :, None] * Vx[:, None, :]).reshape(
+        x_flat.size, n_x_coeffs * n_y_coeffs
+    )
+
+    return basis
+
 def array_normalisation(array):
     """Normalises a increasing array to the span
     of the data such that the normalised array spans
@@ -46,31 +66,55 @@ def array_normalisation(array):
     return normalised_array, array_norm_func
 
 
-def reconstruct_surface(x,
-                        y,
-                        flat_coeffs,
-                        n_x_coeffs,
-                        n_y_coeffs,
-                        x_array_norm_func=None,
-                        y_array_norm_func=None):
-    """Reconstruct surface from x and y values
-    based on xy_array_norm_funcs
-    """
-    if x_array_norm_func is not None:
+# def reconstruct_surface(x,
+#                         y,
+#                         flat_coeffs,
+#                         n_x_coeffs,
+#                         n_y_coeffs,
+#                         x_array_norm_func=None,
+#                         y_array_norm_func=None):
+#     """Reconstruct surface from x and y values
+#     based on xy_array_norm_funcs
+#     """
+#     if x_array_norm_func is not None:
+#         x = x_array_norm_func(x)
+#     if y_array_norm_func is not None:
+#         y = y_array_norm_func(y)
+
+#     basis = construct_basis(x,
+#                             y,
+#                             n_x_coeffs,
+#                             n_y_coeffs)
+#     n_x, n_y = len(x), len(y)
+#     reconstructed_surface = basis @ flat_coeffs
+
+#     reconstructed_surface = np.reshape(reconstructed_surface,
+#                                      shape=(n_y, n_x))
+#     return reconstructed_surface
+
+
+def reconstruct_surface(
+    x,
+    y,
+    flat_coeffs,
+    n_x_coeffs,
+    n_y_coeffs,
+    x_array_norm_func=None,
+    y_array_norm_func=None,
+):
+    """Reconstruct surface from x and y values."""
+
+    # Apply normalization only if needed
+    if x_array_norm_func:
         x = x_array_norm_func(x)
-    if y_array_norm_func is not None:
+    if y_array_norm_func:
         y = y_array_norm_func(y)
 
-    basis = construct_basis(x,
-                            y,
-                            n_x_coeffs,
-                            n_y_coeffs)
-    n_x, n_y = len(x), len(y)
-    reconstructed_surface = basis @ flat_coeffs
+    # Construct basis
+    basis = construct_basis_vectorised(x, y, n_x_coeffs, n_y_coeffs)
 
-    reconstructed_surface = np.reshape(reconstructed_surface,
-                                     shape=(n_y, n_x))
-    return reconstructed_surface
+    # Compute surface directly and reshape
+    return (basis @ flat_coeffs).reshape(len(y), len(x))
 
 def polynomial_basis(freq_array,
                      polynomial_order,
--- gcr/data_processing.py
+++ gcr/data_processing.py
@@ -35,8 +35,7 @@ def create_nw_data_and_covariance_from_raw(waterfall,
     if not isinstance(switch_times, un.Quantity):
         switch_times*=time_unit
 
-    data_waterfall = []
-    noise_var_waterfall = []
+    
 
     n_dicke_sources = len(dicke_switch_targets) + 1 # add one for target itself
 
@@ -59,6 +58,10 @@ def create_nw_data_and_covariance_from_raw(waterfall,
     data_waterfall = np.empty(shape=(n_cycles, len(freqs)))
     covar_waterfall = np.empty(shape=(n_cycles, len(freqs)))
 
+    data_waterfall = []
+    covar_waterfall = []
+    final_times = []
+    final_states = []
 
     for i, (dicke_switches, dicke_times) in enumerate(zip(switch_states, switch_times)):
         gamma_src = np.empty(shape=freqs.shape)
@@ -75,56 +78,91 @@ def create_nw_data_and_covariance_from_raw(waterfall,
         l_temps = 0
         ns_temps = 0
 
-        for j, (state, st_time) in enumerate(zip(dicke_switches, dicke_times)):
-            # pass null states
-            if state not in noise_wave_loads and state != internal_load_label and state != noise_source_label:
-                pass
-            else:
-                if j != n_dicke_sources-1 and i != n_cycles-1:
-                    times_mask = (times > st_time + switch_buffer) &\
-                    (times < dicke_times[j+1] - switch_buffer)
-                elif j == n_dicke_sources-1 and i != n_cycles-1:
-                    times_mask = (times > st_time + switch_buffer) &\
-                    (times < switch_times[i+1, 0] - switch_buffer)
-                # final cycle
-                elif i == n_cycles-1 and j != n_dicke_sources-1:
-                    times_mask = (times > st_time + switch_buffer) &\
-                    (times < dicke_times[j+1] - switch_buffer)
+        # catch loads that are not in the noise wave calibration and skip cycle
+        load_bool = catch_false_loads(dicke_switches, dicke_switch_targets,
+                          noise_wave_loads)
+
+        print(load_bool)
+
+        if catch_false_loads(dicke_switches,
+                             dicke_switch_targets,
+                             noise_wave_loads):
+
+            for j, (state, st_time) in enumerate(zip(dicke_switches, dicke_times)):
+                # pass null states
+                if state not in noise_wave_loads and state != internal_load_label and state != noise_source_label:
+                    pass
                 else:
-                    times_mask = (times > st_time + switch_buffer)
+                    if j != n_dicke_sources-1 and i != n_cycles-1:
+                        times_mask = (times > st_time + switch_buffer) &\
+                        (times < dicke_times[j+1] - switch_buffer)
+                    elif j == n_dicke_sources-1 and i != n_cycles-1:
+                        times_mask = (times > st_time + switch_buffer) &\
+                        (times < switch_times[i+1, 0] - switch_buffer)
+                    # final cycle
+                    elif i == n_cycles-1 and j != n_dicke_sources-1:
+                        times_mask = (times > st_time + switch_buffer) &\
+                        (times < dicke_times[j+1] - switch_buffer)
+                    else:
+                        times_mask = (times > st_time + switch_buffer)
 
-                if state in noise_wave_loads:
-                    final_states.append(state)
-                    p_src_array = waterfall[times_mask]
-                    p_src_times = times[times_mask]
-                    gamma_src = gamma_src_dict[state]
-                    src_temps = source_temperatures[state][times_mask]
+                    if state in noise_wave_loads:
+                        final_states.append(state)
+                        p_src_array = waterfall[times_mask]
+                        p_src_times = times[times_mask]
+                        gamma_src = gamma_src_dict[state]
+                        src_temps = source_temperatures[state][times_mask]
 
-                elif state == internal_load_label:
-                    p_l_array = waterfall[times_mask]
-                    p_l_times = times[times_mask]
-                    l_temps = source_temperatures[state][times_mask]
-                elif state == noise_source_label:
-                    p_ns_array = waterfall[times_mask]
-                    p_ns_times = times[times_mask]
-                    ns_temps = source_temperatures[state][times_mask]
+                    elif state == internal_load_label:
+                        p_l_array = waterfall[times_mask]
+                        p_l_times = times[times_mask]
+                        l_temps = source_temperatures[state][times_mask]
+                    elif state == noise_source_label:
+                        p_ns_array = waterfall[times_mask]
+                        p_ns_times = times[times_mask]
+                        ns_temps = source_temperatures[state][times_mask]
+            
+            data_vector, variance_vector, median_time = return_data_and_variance(p_src_array=p_src_array,
+                                                                                    p_src_array_times=p_src_times,
+                                                                                    t_src_array=src_temps,
+                                                                                    p_l_array=p_l_array,
+                                                                                    p_l_array_times=p_l_times,
+                                                                                    t_l_array=l_temps,
+                                                                                    p_ns_array=p_ns_array,
+                                                                                    p_ns_array_times=p_ns_times,
+                                                                                    t_ns_array=ns_temps,
+                                                                                    gamma_rec=gamma_rec,
+                                                                                    gamma_src=gamma_src,
+                                                                                    freqs=freqs,
+                                                                                    freq_unit=freq_unit,
+                                                                                    time_unit=time_unit,
+                                                                                    temp_sens_variance=assumed_temp_sens_variance)
+            #data_waterfall[i] = data_vector
+            data_waterfall.append(data_vector)
+            #covar_waterfall[i] = variance_vector
+            covar_waterfall.append(variance_vector)
+            #final_times[i] = median_time
+            final_times.append(median_time)
+        else:
+            pass
+    
+    data_waterfall = np.array(data_waterfall)
+    covar_waterfall = np.array(covar_waterfall)
+    final_times = np.array(final_times, dtype=un.Quantity)
 
-        data_vector, variance_vector, median_time = return_data_and_variance(p_src_array=p_src_array,
-                                                                             p_src_array_times=p_src_times,
-                                                                             t_src_array=src_temps,
-                                                                             p_l_array=p_l_array,
-                                                                             p_l_array_times=p_l_times,
-                                                                             t_l_array=l_temps,
-                                                                             p_ns_array=p_ns_array,
-                                                                             p_ns_array_times=p_ns_times,
-                                                                             t_ns_array=ns_temps,
-                                                                             gamma_rec=gamma_rec,
-                                                                             gamma_src=gamma_src,
-                                                                             freqs=freqs,
-                                                                             freq_unit=freq_unit,
-                                                                             time_unit=time_unit,
-                                                                             temp_sens_variance=assumed_temp_sens_variance)
-        data_waterfall[i] = data_vector
-        covar_waterfall[i] = variance_vector
-        final_times[i] = median_time
     return data_waterfall, covar_waterfall, final_times, final_states
+
+def catch_false_loads(switch_states,
+                      dicke_switch_targets,
+                      noise_wave_targets):
+    """Function to identify if a cycle
+    includes a noise-wave target
+    """
+    switch_states = [f for f in switch_states]
+    for ds in dicke_switch_targets:
+        switch_states.remove(ds)
+    
+    if switch_states[0] in noise_wave_targets:
+        return True
+    else:
+        return False
